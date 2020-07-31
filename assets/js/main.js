d8fe465c64b5762ec402795759b8c8848d71b9cf
HovhannesTamoyan <hovhannes.tamoyan@gmail.com>
2020-08-01 02:03:59 +0400

    resources made dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-
-
-
     $('.nav-item a[href^="' + location.pathname.split("/")[location.pathname.split("/").length - 1] + '"]').parent().addClass('active');
-    console.log(location.pathname.split("/")[location.pathname.split("/").length - 1]);
-});
 
-// $(function() {
-// });
+    let refs = $('sup.ref');
+    console.log(refs);
+
+    let resources_list = $('#resource-items')
+
+
+
+    refs.each(id => {
+        let ref = refs[id]
+
+        resources_list.append($('<div> <span class="resources-index">('+(id+1)+')</span> <a target="_blank" href="'+$(ref).attr('src')+'" class="link"> '+$(ref).attr('src')+' </a></div>'))
+        $(ref).text('('+(id+1)+')')
+        console.log(ref)
+    });
+
+});
