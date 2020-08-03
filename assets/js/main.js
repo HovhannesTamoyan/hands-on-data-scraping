c0f18ec22dcbcb98e31a26fee0d489cee9c32314
HovhannesTamoyan <hovhannes.tamoyan@gmail.com>
2020-08-03 12:40:03 +0400

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,33 @@ $( document ).ready(function() {
     // quotetion marking
     texts = $(".txt")
 
+
     texts.each(text_id => {
         txt = $(texts[text_id])
 
         let re = /“(.*?)”/
         var txt_modifs = new Array;
+
+        function find_matches(corpus) {
+            if ((match = re.exec(corpus)) != null && corpus.length != 0) {
+        
+                let start =  match.index
+                let end =  match.index + match[0].length
+        
+        
+                let replicant = "<span class='quote'>"+match[0]+"</span>"
+        
+                let firstPart = corpus.substr(0, start);
+                let lastPart = corpus.substr(end, corpus.length);
+        
+        
+                txt_modifs.push(firstPart + replicant)
+        
+                find_matches(lastPart)
+            }else{
+                txt_modifs.push(corpus)
+            }
+        }
 
         find_matches(txt.html())
 
@@ -37,27 +59,5 @@ function navToggle() {
     x.className += " responsive";
     } else {
     x.className = "nav-container";
-    }
-}
-
-
-function find_matches(corpus) {
-    if ((match = re.exec(corpus)) != null && corpus.length != 0) {
-
-        let start =  match.index
-        let end =  match.index + match[0].length
-
-
-        let replicant = "<span class='quote'>"+match[0]+"</span>"
-
-        let firstPart = corpus.substr(0, start);
-        let lastPart = corpus.substr(end, corpus.length);
-
-
-        txt_modifs.push(firstPart + replicant)
-
-        find_matches(lastPart)
-    }else{
-        txt_modifs.push(corpus)
     }
 }
